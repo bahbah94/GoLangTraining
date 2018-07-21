b3bbd42f2c5f707797d8740d8cbcfa478e589a57
Randall Lionel Kharkrang <bahduh94@gmail.com>
2018-07-21 19:17:03 +0530

    variablesshort hand

---
+++ variables/shorthand/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "golang"
+	c := 4.70
+	d := false
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+}
