4ae080c647f6620372d65bd58096dc69a6c7b1bd
Randall Lionel Kharkrang <bahduh94@gmail.com>
2018-07-21 19:26:42 +0530

    varzero

---
+++ variables/lessemphasis/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "golang"
+	c := 4.70
+	d := false
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+}
+
---
+++ variables/varzero/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b string
+	var c float32
+	var d bool
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+}
